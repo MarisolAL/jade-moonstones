d4d92b52dfa5639d35b7c835011179f1050aebb2
Marisol Amezcua <36975246+MarisolAL@users.noreply.github.com>
2025-09-07 19:19:49 -0600

    WIP: Add music catalog and rendering functions

--- src/jade_moonstones/music/views.cljs
+++ src/jade_moonstones/music/views.cljs
@@ -1,9 +1,73 @@
 (ns jade-moonstones.music.views
   (:require
+   [jade-moonstones.music-catalog :as music-cat]
    [reagent.core :as rg] 
    [re-frame.core :as rf]))
 
+(defn youtube-embed
+  "Returns the embeded track component of a song in Youtube"
+  [song]
+  (let [allow-map {:accelerometer      true
+                   :autoplay           true
+                   :clipboard-write    true
+                   :gyroscope          true
+                   :picture-in-picture true
+                   :web-share          true}
+        yt-link   (str "https://www.youtube.com/embed/" (:youtube-link song))]
+    [:iframe {:width           560
+              :height          315
+              :src             yt-link
+              :title           (:name song)
+              :frameBorder     0
+              :allow           allow-map
+              :allowFullScreen true
+              :referrerPolicy  "strict-origin-when-cross-origin"}]))
+
+(defn spotify-embed
+  "Returns the embeded track component of a song in Spotify"
+  [song]
+  (let [allow-map    {:autoplay           true
+                      :clipboard-write    true
+                      :fullScreen         true 
+                      :picture-in-picture true}
+        spotify-link (str "https://open.spotify.com/embed/track/" (:spotify-link song)
+                          "?utm_source=generator")]
+    [:iframe {:style           {:border-radius "12px"} 
+              :src             spotify-link
+              :width           560
+              :height          315
+              :frameBorder     0
+              :allowFullScreen ""
+              :allow           allow-map
+              :loading         :lazy}]))
+
+(defn song-card [song]
+  (let
+      [show-yt?      (rg/atom false)
+       show-spotify? (rg/atom false)]
+    (fn [song]
+      (let [has-yt?      (not (clojure.string/blank? (:youtube-link song)))
+            has-spotify? (not (clojure.string/blank? (:spotify-link song)))]
+        [:div {:style
+               {:border "5px aliceblue"}}
+         [:p (:name song)]
+         [:p (:artist song)]
+         (if has-yt?
+           [:div
+            [:button {:on-click #(swap! show-yt? not)} "Show YT!"]
+            (if @show-yt?
+              (youtube-embed song))])
+         (if has-spotify?
+           [:div
+            [:button {:on-click #(swap! show-spotify? not)} "Show Spotify!"]
+            [:div
+             (if @show-spotify?
+               (spotify-embed song))]])]))))
+
 (defn page []
   [:div
-   [:h2 "🎶 Music findings~"]])
+   [:h2 "🎶 Music findings~"] 
+   (for [song music-cat/music-catalog]
+     ^{:key (str (:title song) "-" (:name song))} 
+     [song-card song])])
 
---
+++ src/jade_moonstones/music_catalog.cljs
@@ -0,0 +1,53 @@
+(ns jade-moonstones.music-catalog)
+
+(def music-catalog
+  [{:name "エレボス"
+    :artist "EN0N"
+    :year "2022"
+    :youtube-link "P9ejlUnedCQ?si=AjSovkz0RiDEOEOR"
+    :spotify-link ""}
+   {:name "On The Run"
+    :artist "Empire Fall"
+    :year "2024"
+    :youtube-link "4m5bRF8NINU?si=58c2fQVhOd7I7_r9"
+    :spotify-link "0kKL9D1iaI5Hr4jbEZMjon"}
+   {:name "FLATLINE"
+    :artist "Blind Channel"
+    :year "2023"
+    :youtube-link "GIJE6VYsQHw?si=HVJReG1X6_6bMYhG"
+    :spotify-link "1YoyRX9eqzpRZjqpykA0qB"}
+   {:name "Blood Diamondz"
+    :artist "Sniper & Sexion D'assaut"
+    :year "2011"
+    :youtube-link "Johr9g8oDq8?si=B5hVIk1AKxNYDuUe"
+    :spotify-link ""}
+   {:name "别让爱凋落"
+    :artist "好听吗 & Tz安竹"
+    :year "2025"
+    :youtube-link "X9_vAz7Zq-o?si=2_F_IZ3v6QQu9oHD"
+    :spotify-link "5yOcA3bDEMVu6MUjPDBEXB"}
+   {:name "SAUSERYKO"
+    :artist "Ashemez"
+    :year ""
+    :youtube-link "f6bdQOj9zkg?si=9nwNvLA_uEo29cG3"
+    :spotify-link "7mPZAs7LUOBaLtTEUib27h"}
+   {:name "662"
+    :artist "Christone \"Kingfish\" Ingram"
+    :year "2021"
+    :youtube-link "QdC99sQRd1s?si=mGjRqlDbs0Rqra0z"
+    :spotify-link "7lM1cf3zZuish0TjRnn5MB"}
+   {:name "Bacardi" 
+    :artist "Masta Wu & Red-Roc"
+    :year "2007"
+    :youtube-link "oMFk_Zzs0jM?si=ea0MAezcrxx8AQnl"
+    :spotify-link "6Zj3RrTjnq374KMbMI1yPH"
+    }
+  
+   ]) 
+
+
+ #_{:name "" 
+    :artist ""
+    :year ""
+    :youtube-link ""
+    :spotify-link ""}
